f5ef5b9c465b352b16ed4c7ab3c34fc8b51b761c
Harsh <harshrohillarohilla199@gmail.com>
2024-09-02 11:35:08 +0530

    check

---
+++ app/src/main/java/com/example/recipeapp/RecipeScreen.kt
@@ -0,0 +1,92 @@
+package com.example.recipeapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier,navigateToDetail:(Category) -> Unit,
+                 viewstate:MainViewModel.RecipeState)
+{
+
+
+    Box(modifier = Modifier.fillMaxSize())
+    {
+        when
+        {
+            viewstate.loading ->
+            {
+                CircularProgressIndicator(modifier.align(Alignment.Center))
+            }
+            viewstate.error!=null ->
+            {
+                Text("Error occured")
+            }
+            else->
+            {
+                categoryScreen(categories = viewstate.list,navigateToDetail)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun categoryScreen(categories:List<Category>,navigateToDetail:(Category) -> Unit)
+{
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
+
+        items(categories){
+                category->
+            categoryItem(category = category,navigateToDetail)
+        }
+
+    }
+
+}
+
+@Composable
+fun categoryItem(category: Category,
+                 navigateToDetail:(Category) -> Unit)
+{
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp).clickable { navigateToDetail(category) },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+            , contentDescription = null )
+
+        Text(
+            text = category.strCategory,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
+
+    }
+}
